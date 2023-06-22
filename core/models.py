ee4b313cf1bd6e4f75046ab7d5a81d515bc763f0
Murat2101 <137098994+Murat2101@users.noreply.github.com>
2023-06-22 22:14:27 +0600

    worker

--- core/models.py
+++ core/models.py
@@ -4,9 +4,14 @@ from django.db import models
 class Vacancy(models.Model):
     title = models.CharField(max_length=255)
     salary = models.IntegerField(null=True, blank=True)
-    description = models.TextField(default='Нет описантя')
+    description = models.TextField(default='Нет описания')
     is_relevant = models.BinaryField(default=True)
     email = models.EmailField()
     contacts = models.CharField(max_length=100, verbose_name='Контакты')
     def __str__(self):
         return self.title
+
+class Company(models.Model):
+    title = models.CharField(max_length=255)
+    address = models.CharField(max_length=150)
+    employees = models.IntegerField(null=True, blank=True)
